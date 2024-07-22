be47bc8c428bfc6c760ce8cc54aab4af18b721ce
Claire Schlesinger <esslushy@gmail.com>
2024-07-22 14:54:04 -0500

    get id needed

--- src/StoppableThread.py
+++ src/StoppableThread.py
@@ -2,8 +2,16 @@ import threading
 import ctypes
  
 class StoppableThread(threading.Thread):
+    def get_id(self):
+        # returns id of the respective thread
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for id, thread in threading._active.items():
+            if thread is self:
+                return id
+ 
     def stop(self):
-        thread_id = threading.get_ident(self)
+        thread_id = self.get_id()
         res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id,
               ctypes.py_object(SystemExit))
         if res > 1:
