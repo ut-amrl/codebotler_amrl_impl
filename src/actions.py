3aee35b51125c21d32453302b07b0c73933d6a51
Claire Schlesinger <esslushy@gmail.com>
2024-08-21 12:44:25 -0500

    debugging

--- src/actions.py
+++ src/actions.py
@@ -214,8 +214,6 @@ class RobotActions:
         msg = String()
         msg.data = message
         self.robot_say_pub.publish(msg)
-        self.say_server.set_succeeded()
-        return
         print(f"Robot says: \"{message}\"")
         word_len = len(message.split(" "))
         for _ in range(self.DATA['SLEEP_AFTER_SAY'] * word_len * 2 / self.DATA['SLEEP_BETWEEN_CHECKS']):
@@ -224,6 +222,8 @@ class RobotActions:
                 self.say_server.set_preempted()
                 success = False
                 return
+        self.say_server.set_succeeded()
+        return
         if self.say_server.is_preempt_requested():
             self.say_server.set_preempted()
             success = False
